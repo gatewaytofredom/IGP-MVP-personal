9450d0c6d2636dc481cb9f26337745567b6a2ff8
gatewaytofredom <bobdosgames@yahoo.com>
2018-03-16 09:50:39 -0600

    Adds terrain textures

--- Assets/Camerafallow.cs
+++ Assets/Camerafallow.cs
@@ -7,7 +7,7 @@ public class Camerafallow : MonoBehaviour {
     public float smoothTime = 0.001F;
     private Vector3 velocity = Vector3.zero;
     private int xOffset = 0;
-    private int yOffset = -10;
+    private int yOffset = -3;
     private int zOffset = 0;
     private float rotation = 0;
     // Use this for initialization
@@ -21,7 +21,7 @@ public class Camerafallow : MonoBehaviour {
         transform.LookAt(target);
 
 
-        Vector3 targetPosition = target.TransformPoint(new Vector3(0+xOffset, 5+yOffset, 0+zOffset));
+        Vector3 targetPosition = target.TransformPoint(new Vector3(0+xOffset, 0+yOffset, 0+zOffset));
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
        
     }

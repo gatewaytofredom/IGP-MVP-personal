7d651eb92cf00f2a317627cd8be1e80eedc7b096
gatewaytofredom <bobdosgames@gmail.com>
2018-03-16 01:50:38 -0500

    Adds basic player movemnt on the x and z axis
    weeee

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    // Use this for initialization
+    float playerX = 0;
+    float playerY = 0;
+    void Start () {
+ 
+}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = -(Input.GetAxis("Mouse X")/2);
+        float y = Input.GetAxis("Mouse Y")/2;
+
+        playerX = playerX + x/2;
+        playerY = playerY + y/2;
+
+        transform.position = new Vector3(playerX,playerY,0);
+    }
+}

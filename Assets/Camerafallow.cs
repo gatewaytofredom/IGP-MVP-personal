2fe16cd4a542696747480d6bfcc78b156a66aaa5
gatewaytofredom <bobdosgames@yahoo.com>
2018-03-16 09:15:10 -0600

    Adds rotation + Terrain
    Need to fix camera rotation.

---
+++ Assets/Camerafallow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camerafallow : MonoBehaviour {
+    public Transform target;
+    public float smoothTime = 0.001F;
+    private Vector3 velocity = Vector3.zero;
+    private int xOffset = 0;
+    private int yOffset = -10;
+    private int zOffset = 0;
+    private float rotation = 0;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            rotation -= 20f;
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            rotation += 20f;
+        }
+
+        rotation *= Time.deltaTime;
+
+        Vector3 targetPosition = target.TransformPoint(new Vector3(0+xOffset, 5+yOffset, 0+zOffset));
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        transform.Rotate(0, rotation, 0);
+    }
+}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,18 +6,39 @@ public class PlayerMovement : MonoBehaviour {
     // Use this for initialization
     float playerX = 0;
     float playerY = 0;
+    float playerZ = 0;
+    float playerRotation = 180;
+    float playerTranslation = 0;
     void Start () {
  
 }
 	
 	// Update is called once per frame
 	void Update () {
-        float x = -(Input.GetAxis("Mouse X")/2);
-        float y = Input.GetAxis("Mouse Y")/2;
+        playerTranslation *= Time.deltaTime;
+        playerRotation *= Time.deltaTime;
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            playerRotation -= 1f;
+        }else if (Input.GetAxis("Horizontal") < 0)
+        {
+            playerRotation += 1f;
+        }
 
-        playerX = playerX + x/2;
-        playerY = playerY + y/2;
 
-        transform.position = new Vector3(playerX,playerY,0);
+        if(Input.GetAxis("Vertical") > 0)
+        {
+            playerTranslation += 0.2f;
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            playerTranslation -= 0.2f;
+        }
+
+
+
+        // transform.position = new Vector3(playerX,0, playerZ);
+        transform.Translate(0,0,playerTranslation);
+        transform.Rotate(0,playerRotation,0);
     }
 }
